8dde18740567b410a1047a07464aee05a4b97d17
Yevhen <benatanlev@icloud.com>
2025-06-09 23:55:54 +0300

    1

--- src/API/campersApi.js
+++ src/API/campersApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io";
 
-export const fetchCampers = async (page = 1, limit = 6) => {
+export const fetchCampers = async (page = 1, limit = 3) => {
   const response = await axios.get(`${BASE_URL}/campers`, {
     params: {
       page,
